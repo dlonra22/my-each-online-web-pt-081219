3537bf2fd90cb244e77c01417078620f4f480cc8
arnie b <arnold.bwaila@gmail.com>
2019-08-24 11:08:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,10 +3,11 @@ def my_each(array) # put argument(s) here
   if block_given?
     i = 0 
     while i < array.length 
-    yield array[i]
-    i += 1
+      yield array[i]
+      i += 1
+    end
+    array
+  else 
+    puts "block not given"
   end
-  array
-else 
-  puts "block not given"
 end
